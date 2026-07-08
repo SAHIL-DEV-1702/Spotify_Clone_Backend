9162e1b0a8543751b3eeba8d24748b0087e2dacb
Sahil Patil <patilsahil@172002@gmail.com>
2026-07-08 16:00:55 +0530

    Refactor createMusic function to clean up commented code and improve readability

--- src/controller/music.controller.js
+++ src/controller/music.controller.js
@@ -6,7 +6,7 @@ const createMusic = async (req, res) => {
 
     const token = req.cookies.token
 
-    console.log(token)
+    // console.log(token)
 
     if (!token) {
         return res.status(401).json({ message: "Unauthorized" })
@@ -18,25 +18,37 @@ const createMusic = async (req, res) => {
             return res.status(403).json({ message: "You Dont Have Access To Create Music" })
         }
 
+
+        const { title, } = req.body
+        const file = req.file
+        console.log(file, "file")
+        const result = await uploadMusic(file.buffer)
+
+        // console.log(result, "result of file upload")
+
+        const newMusic = new MusicModel({
+            title,
+            url: result.url,
+            artist: decoded.id
+        })
+
+        res.status(201).json({
+            message: "Music Created Successfully", music: {
+                _id: newMusic._id,
+                title: newMusic.title,
+                url: newMusic.url,
+                artist: newMusic.artist
+            }
+        })
+
+
     } catch (error) {
         res.status(401).json({ message: "Invalid Token" })
-        console.log(error)
     }
 
-    const { title, } = req.body
-    const { file } = req.files
 
-    const result = await uploadMusic(file.data)
-
-    const newMusic = new MusicModel({
-        title,
-        url: result.url,
-        artist: decoded.id
-    })
-
-    res.status(201).json({ message: "Music Created Successfully", music: newMusic })
 
 
 }
 
-module.exports = { createMusic } 
+module.exports = createMusic  
